fb96b369ccd971a4dbb81271e5d00cd4ff72c942
Satrio <satriotol69@gmail.com>
2020-04-01 18:18:20 +0700

    urls image

--- anlos/urls.py
+++ anlos/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from project import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name='list'),
     path('admin/', admin.site.urls),
     # path('',views.SchoolListView.as_view(),name='list'),
-
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
