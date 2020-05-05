e9c328eabb531da82c833551f436f24e12a2a332
Satrio <satriotol69@gmail.com>
2020-05-05 22:13:58 +0700

    buat perusahaan coba

--- perusahaan/views.py
+++ perusahaan/views.py
@@ -49,6 +49,35 @@ def register(request):
 
     return render(request,'login/registration.html',{'user_form':user_form,'profile_form':profile_form,'registered':registered})
 
+def buat(request):
+    registered = False
+    
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserPerusahaanInfo(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserPerusahaanInfo()
+
+    return render(request,'perusahaan/buatperusahaan.html',{'user_form':user_form,'profile_form':profile_form,'registered':registered})
+
 def user_login(request):
     context_object_name = 'data_login'
     if request.method == 'POST':
