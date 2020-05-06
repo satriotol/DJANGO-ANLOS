e02f458a3e5f17386a6d9daed8233914c329f86c
Satrio <satriotol69@gmail.com>
2020-05-06 22:57:54 +0700

    geolocations

---
+++ static/js/script.js
@@ -0,0 +1,52 @@
+//var myPolygon;
+function initialize() {
+  // Map Center
+  var myLatLng = new google.maps.LatLng(-6.96697, 110.39947);
+  // General Options
+  var mapOptions = {
+    zoom: 12,
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.RoadMap
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  // Polygon Coordinates
+  var triangleCoords = [
+    new google.maps.LatLng(-6.96697, 110.39947),
+    new google.maps.LatLng(-7.05489, 110.41491),
+    new google.maps.LatLng(-7.00378, 110.47654),
+  ];
+  // Styling & Controls
+  myPolygon = new google.maps.Polygon({
+    paths: triangleCoords,
+    draggable: true, // turn off if it gets annoying
+    editable: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+
+  myPolygon.setMap(map);
+  //google.maps.event.addListener(myPolygon, "dragend", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "insert_at", getPolygonCoords);
+  //google.maps.event.addListener(myPolygon.getPath(), "remove_at", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "set_at", getPolygonCoords);
+}
+
+//Display Coordinates below map
+function getPolygonCoords() {
+  var len = myPolygon.getPath().getLength();
+  var htmlStr = "";
+  for (var i = 0; i < len; i++) {
+    // htmlStr += "new google.maps.LatLng(" + myPolygon.getPath().getAt(i).toUrlValue(5) + "), ";
+    htmlStr += "(" + myPolygon.getPath().getAt(i).toUrlValue(5) + "), ";
+    //Use this one instead if you want to get rid of the wrap > new google.maps.LatLng(),
+    //htmlStr += "" + myPolygon.getPath().getAt(i).toUrlValue(5);
+  }
+  document.getElementById('info').innerHTML = htmlStr;
+}
+
+function copyToClipboard(text) {
+  window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
