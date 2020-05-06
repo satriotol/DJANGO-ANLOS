bba9e0d1d2483fa995098c7762d5b4ab009f3667
Satrio <satriotol69@gmail.com>
2020-05-06 23:06:16 +0700

    polygon geolocation

--- static/js/script.js
+++ static/js/script.js
@@ -1,52 +1,81 @@
-//var myPolygon;
-function initialize() {
-  // Map Center
-  var myLatLng = new google.maps.LatLng(-6.96697, 110.39947);
-  // General Options
-  var mapOptions = {
-    zoom: 12,
-    center: myLatLng,
-    mapTypeId: google.maps.MapTypeId.RoadMap
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  // Polygon Coordinates
-  var triangleCoords = [
-    new google.maps.LatLng(-6.96697, 110.39947),
-    new google.maps.LatLng(-7.05489, 110.41491),
-    new google.maps.LatLng(-7.00378, 110.47654),
-  ];
-  // Styling & Controls
-  myPolygon = new google.maps.Polygon({
-    paths: triangleCoords,
-    draggable: true, // turn off if it gets annoying
-    editable: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35
-  });
-
-  myPolygon.setMap(map);
-  //google.maps.event.addListener(myPolygon, "dragend", getPolygonCoords);
-  google.maps.event.addListener(myPolygon.getPath(), "insert_at", getPolygonCoords);
-  //google.maps.event.addListener(myPolygon.getPath(), "remove_at", getPolygonCoords);
-  google.maps.event.addListener(myPolygon.getPath(), "set_at", getPolygonCoords);
+if(!!navigator.geolocation) {
+	    	
+  var map;
+  var geocoder;
+  var polygonArray = [];
+	var mapOptions = {
+		    		zoom: 15,
+		    		mapTypeId: google.maps.MapTypeId.ROADMAP
+		    	};
+	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	navigator.geolocation.getCurrentPosition(function(position) {
+	    		
+		    		var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		    		
+		    		var infowindow = new google.maps.InfoWindow({
+		    			map: map,
+		    			position: geolocate,
+		    			content:
+		    				'<h4>Location pinned from HTML5 Geolocation!</h4>' +
+		    				'<p>Latitude: ' + position.coords.latitude + '</p>' +
+		    				'<p>Longitude: ' + position.coords.longitude + '</p>'
+		    		});
+		    		
+		    		map.setCenter(geolocate);
+		    		
+	    		});
+	    		
+} 
+else{
+  document.getElementById('map_canvas').innerHTML = 'No Geolocation Support.';
 }
 
-//Display Coordinates below map
-function getPolygonCoords() {
-  var len = myPolygon.getPath().getLength();
-  var htmlStr = "";
-  for (var i = 0; i < len; i++) {
-    // htmlStr += "new google.maps.LatLng(" + myPolygon.getPath().getAt(i).toUrlValue(5) + "), ";
-    htmlStr += "(" + myPolygon.getPath().getAt(i).toUrlValue(5) + "), ";
-    //Use this one instead if you want to get rid of the wrap > new google.maps.LatLng(),
-    //htmlStr += "" + myPolygon.getPath().getAt(i).toUrlValue(5);
-  }
-  document.getElementById('info').innerHTML = htmlStr;
+function initializeDrawer() {
+    var drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.POLYGON,
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [
+            google.maps.drawing.OverlayType.MARKER,
+            google.maps.drawing.OverlayType.CIRCLE,
+            google.maps.drawing.OverlayType.POLYGON,
+            google.maps.drawing.OverlayType.POLYLINE,
+            google.maps.drawing.OverlayType.RECTANGLE]
+        },
+        markerOptions: {
+            icon: 'images/car-icon.png'
+        },
+        circleOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1
+        },
+        polygonOptions: {
+            fillColor: '#BCDCF9',
+            fillOpacity: 0.5,
+            strokeWeight: 2,
+            strokeColor: '#57ACF9',
+            clickable: false,
+            editable: false,
+            zIndex: 1
+        }
+    });
+    console.log(drawingManager)
+    drawingManager.setMap(map)
+
+    google.maps.event.addListener(drawingManager, 'polygoncomplete', function (polygon) {
+        document.getElementById('info').innerHTML;
+        for (var i = 0; i < polygon.getPath().getLength(); i++) {
+            document.getElementById('info').innerHTML += "" + polygon.getPath().getAt(i).toUrlValue(6) + ";";
+        }
+        polygonArray.push(polygon);
+    });
+
 }
 
-function copyToClipboard(text) {
-  window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
-}
+// Initialize the drawer tool
+google.maps.event.addDomListener(window, "load", initializeDrawer);
